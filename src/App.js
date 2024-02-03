b7630ab080be4b3e0fa164851a38d459abbc0e8c
rhkearns <ryankearns217@gmail.com>
2024-02-03 17:10:31 -0500

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import Navbar from './components/Navbar/Navbar';
+import StoryPage from './pages/StoryPage/StoryPage';
+import Directions from './pages/Directions/Directions';
+import Schedule from './pages/Schedule/Schedule';
+import Faqs from './pages/Faqs/Faqs';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/story" element={<StoryPage />} />
+        <Route path="/directions" element={<Directions />} />
+        <Route path="/schedule" element={<Schedule /> } />
+				<Route path="/faqs" element={<Faqs />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
