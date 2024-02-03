257c8a7962edc9057a44809ab19d2bf6592802b4
rhkearns <ryankearns217@gmail.com>
2024-02-03 17:11:12 -0500

    stub up homepage, add basics

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Samantha Yudin <span>and</span> Ryan Kearns</h1>
+      <h2>April 26, 2025</h2>
+      <h2>Peekskill, NY</h2>
+    </>
+  )
+}
+
+export default HomePage
