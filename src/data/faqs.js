c4d8145abffe7b63cecb63957eef66e005e9b0b7
rhkearns <ryankearns217@gmail.com>
2024-02-04 11:34:20 -0500

    add faqs

---
+++ src/data/faqs.js
@@ -0,0 +1,19 @@
+let questions = [
+	{
+		question: 'Where is the Factoria at Charles Point Located?',
+		answer:
+			'The Factoria at Charles Point is in Peekskill, NY. It is an hour to an hour and 10-minute train ride on Metro North from Grand Central and a 4-minute drive from the train station (about $10-$15). Via car, it is located off the Louisa Street exit on US-9 N. It is about a 35-minute drive from the Westchester Country Airport.',
+	},
+	{
+		question: 'Can we bring our kids?',
+		answer:
+			'While we love all of your children, our wedding will be child-free. Enjoy the night while the kids are at home with a sitter!',
+	},
+	{
+		question: 'I have a food allergy, can I make a special request?',
+		answer:
+			'Yes! Please let Sam and Ryan know of your food allergy/dietary restrictions when you RSVP!',
+	},
+];
+
+export { questions };
