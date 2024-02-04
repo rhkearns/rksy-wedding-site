b484f09ee90a2b3aeb933477f82155e24e79584d
rhkearns <ryankearns217@gmail.com>
2024-02-04 12:38:23 -0500

    add events doc

---
+++ src/data/schedule.js
@@ -0,0 +1,18 @@
+let events = [
+  {
+    title: "Rehearsal Dinner",
+    date: "Fri, Apr 25, 2025",
+    time: '7:00pm',
+    open: false,
+    location: null
+  },
+  {
+    title: "Samantha and Ryan's Wedding",
+    date: "Sat, Apr 26, 2025",
+    time: "4:00pm to 10:00pm",
+    open: true,
+    location: "Factoria at Charles Point"
+  }
+]
+
+export default events
