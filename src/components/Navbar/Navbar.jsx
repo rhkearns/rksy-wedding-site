224e46b30901b7fcee082f1e2859125d05ea2ede
rhkearns <ryankearns217@gmail.com>
2024-03-14 19:08:59 -0400

    nav bar disappears, more style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import './navbar.scss';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
@@ -10,22 +10,34 @@ const Navbar = () => {
 		navRef.current.classList.toggle('responsive_nav');
 	};
 
+	// const hideNavbar = () => {
+	// 	navRef.current.classList.remove('responsive_nav');
+	// };
+
 	return (
 		<header>
 			<nav ref={navRef} className="nav-bar">
-				<NavLink to="/" className="nav-link">
+				<NavLink to="/" className="nav-link" onClick={showNavbar}>
 					Home
 				</NavLink>
-				<NavLink to="/story" className="nav-link">
+				<NavLink to="/story" className="nav-link" onClick={showNavbar}>
 					Our Story
 				</NavLink>
-				<NavLink to="/directions" className="nav-link">
+				<NavLink to="/directions" className="nav-link" onClick={showNavbar}>
 					Directions
 				</NavLink>
-				<NavLink to="/schedule" className="nav-link">
+				<NavLink to="/schedule" className="nav-link" onClick={showNavbar}>
 					Schedule
 				</NavLink>
-				<NavLink to="/faqs" className="nav-link">
+				<a
+					href="https://www.zola.com/registry/ryanandsam2025"
+					target="_blank"
+					rel="noopener noreferrer"
+					onClick={showNavbar}
+				>
+					Registry
+				</a>
+				<NavLink to="/faqs" className="nav-link" onClick={showNavbar}>
 					FAQs
 				</NavLink>
 				<button className="nav-btn nav-close-btn" onClick={showNavbar}>
