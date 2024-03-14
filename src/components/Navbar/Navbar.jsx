e61189c89b04d5c2e4b68c7df183d5c17de3a2e3
rhkearns <ryankearns217@gmail.com>
2024-03-14 19:13:25 -0400

    clean up

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import './navbar.scss';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
