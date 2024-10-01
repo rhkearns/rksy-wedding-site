e800fa7117bbf0adb659ad4859918b06237edadc
rhkearns <ryankearns217@gmail.com>
2024-10-01 19:36:48 -0400

    add email contact

--- src/pages/Faqs/Faqs.jsx
+++ src/pages/Faqs/Faqs.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import "./Faqs.scss";
+import './Faqs.scss';
 import questions from '../../data/faqs';
 import Question from '../../components/Question/Question';
 
 const Faqs = () => {
 	return (
-		<div className='questions'>
+		<div className="questions">
 			{questions.map(q => (
-        <Question question={q.question} answer={q.answer} />
+				<Question question={q.question} answer={q.answer} />
 			))}
+			<h4>
+				If you have any other questions, feel free to email Sam and Ryan:{' '}
+				<a
+					href="mailto:rksy18371@gmail.com"
+					target="_blank"
+					rel="noopener noreferrer"
+					id="email-link"
+				>
+					rksy18371@gmail.com
+				</a>
+			</h4>
 		</div>
 	);
 };
