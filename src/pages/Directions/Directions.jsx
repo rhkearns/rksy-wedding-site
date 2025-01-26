36c97d8eaa6e040e5829e9df2ba04b52b699c9c2
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2025-01-26 09:33:33 -0500

    fix link

--- src/pages/Directions/Directions.jsx
+++ src/pages/Directions/Directions.jsx
@@ -42,7 +42,15 @@ const Directions = () => {
 								A room block is available for the night of Saturday, April 26.
 								Please select "Group Rate" in the "Rate Preference dropdown and
 								enter code "YKW" to receive the discounted price.
-								** If you have trouble booking a room as part of the room block, please send us an <a href="mailto:rksy18371@gmail.com">email</a> 
+								** If you have trouble booking a room as part of the room block, please send us an 
+								<a
+									href="mailto:rksy18371@gmail.com"
+									target="_blank"
+									rel="noopener noreferrer"
+									id="email-link"
+								>
+								email
+								</a> 
 							</p>
 							<a 
 								href="https://www.hiexpress.com/peekskillny" 
